da474ee5130dda8dcb4c8a4f711a1a36dacb5238
NoraNora <noranora@wp-works.com>
2019-05-17 11:55:28 +0900

    update

---
+++ tests/test-sample.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Nora_Ace
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+
+	
+	
+}
+
+
