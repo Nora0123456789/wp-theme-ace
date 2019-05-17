1b6670c26c69069fcedc973c7df248ace50f50aa
NoraNora <noranora@wp-works.com>
2019-05-17 11:36:26 +0900

    init

---
+++ tests/test-data.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Nora_Ace
+ */
+
+/**
+ * Data test case.
+ */
+class DataTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_data() {
+
+        
+
+    }
+
+	
+	
+}
+
+
---
+++ tests/test-rendering.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Nora_Ace
+ */
+
+/**
+ * Sample test case.
+ */
+class RenderingTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_header() {
+
+        ace()->getWidgetAreaManager();
+
+
+    }
+
+	
+	
+}
+
+
